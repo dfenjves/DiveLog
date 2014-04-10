367d1f1701268612845c69730b9add76e73678be
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-04-10 13:51:45 -0400

    multiple file and js uploads working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery-fileupload
+//= require jquery-fileupload/basic
 //= require_tree .
 
 jQuery(document).ready(function($) {
--- app/controllers/concerns/photos_controller.rb
+++
@@ -1,7 +0,0 @@
-class PhotosController < ApplicationController
-  
-  def create
-  	@photo = Photo.create(:image => params[:image], :dive_id => params[:photo][:dive_id])
-  end
-
-end
